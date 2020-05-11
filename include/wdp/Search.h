1a1ac0f42058945ad9c68e337a9bcd0579bb6b53
lewis-revill <lewis.revill@embecosm.com>
2020-05-11 19:48:32 +0100

    [WDP][Search] Rewrite sumOfSquaredDifferences & add getBlock
    This change renames the SumOfSquaredDifferences function to start with a
    lower case, and rewrites it such that it correctly operates on all
    channels of the input image view. The function getBlock is added, which
    creates a block of pixels with given radii surrounding the given
    coordinates.

--- include/wdp/Search.h
+++ include/wdp/Search.h
@@ -18,36 +18,66 @@
 #define WDP_SEARCH_H
 
 #include <boost/concept_check.hpp>
-#include <boost/gil/gil_all.hpp>
+#include <boost/gil/image.hpp>
+
 #include <cstddef>
 
 namespace wdp {
 
 /// Calculate the sum of squared differences between the pixels in two blocks.
-template<typename LHSView, typename RHSView>
-std::size_t SumOfSquaredDifferences(const LHSView &LHS, const RHSView &RHS) {
+template <typename LHSView, typename RHSView>
+std::size_t sumOfSquaredDifferences(const LHSView &LHS, const RHSView &RHS) {
   // Input arguments must be two compatible 2D image views.
-  boost::function_requires<boost::gil::RandomAccess2DImageViewConcept<LHSView>>();
-  boost::function_requires<boost::gil::RandomAccess2DImageViewConcept<RHSView>>();
+  boost::function_requires<
+      boost::gil::RandomAccess2DImageViewConcept<LHSView>>();
+  boost::function_requires<
+      boost::gil::RandomAccess2DImageViewConcept<RHSView>>();
 
-  boost::function_requires<boost::gil::ViewsCompatibleConcept<LHSView, RHSView>>();
+  boost::function_requires<
+      boost::gil::ViewsCompatibleConcept<LHSView, RHSView>>();
 
   // Can only operate on views of equal dimensions.
   assert(LHS.dimensions() == RHS.dimensions());
 
   // TODO: Consider parallelisation.
   std::size_t Sum = 0;
-  for (std::ptrdiff_t Y = 0; Y < LHS.height(); ++Y) {
-    auto LHSIt = LHS.row_begin(Y);
-    auto RHSIt = RHS.row_begin(Y);
-    for (std::ptrdiff_t X = 0; X < LHS.width(); ++X) {
-      auto Difference = LHSIt[X] - RHSIt[X];
-      Sum += Difference * Difference;
+  for (std::size_t Y = 0; Y < LHS.height(); ++Y) {
+    typename LHSView::x_iterator LHSIt = LHS.row_begin(Y);
+    typename RHSView::x_iterator RHSIt = RHS.row_begin(Y);
+    for (std::size_t X = 0; X < LHS.width(); ++X) {
+      for (std::size_t C = 0; C < boost::gil::num_channels<LHSView>::value;
+           ++C) {
+        const auto Difference = LHSIt[X][C] - RHSIt[X][C];
+        Sum += Difference * Difference;
+      }
     }
   }
   return Sum;
 }
 
-} // namespace wdc
+/// Get a block of pixels with given radii surrounding the given coordinates.
+template <typename Image, typename Coordinates, typename Radii>
+const typename Image::view_t getBlock(const Image &Img, const Coordinates &C,
+                                      const Radii &R) {
+  boost::function_requires<boost::gil::RandomAccess2DImageConcept<Image>>();
+  boost::function_requires<boost::gil::Point2DConcept<Coordinates>>();
+  boost::function_requires<boost::gil::Point2DConcept<Radii>>();
+
+  typedef typename Image::view_t View;
+  typedef typename Image::point_t Point;
+  typedef typename View::xy_locator Locator;
+
+  // Create the dimensions of the block.
+  const Point Dimensions(R.x * 2 + 1, R.y * 2 + 1);
+
+  // Get a locator for the beginning of the block.
+  const Point Begin(C.x - R.x, C.y - R.y);
+  const Locator &Loc = Img._view.xy_at(Begin);
+
+  // Construct a new view into the image.
+  return View(Dimensions, Loc);
+}
+
+} // namespace wdp
 
 #endif // WDP_SEARCH_H
