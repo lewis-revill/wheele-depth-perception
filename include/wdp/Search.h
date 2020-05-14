26e0ee0557664994ae8aaed3b21f80ec4a7c5723
lewis-revill <lewis.revill@embecosm.com>
2020-05-14 15:18:46 +0100

    [WDP][Search] Add findBestMatch & other small interface changes
    This adds the function findBestMatch to the Search module which searches
    through a second image to find a neighbourhood which has the lowest sum
    of squared differences to a neighbourhood in the first image.
    
    In addition the wording of 'block' has been changed to 'neighbourhood'
    throughout, code has been made more readable, and some assertions were
    added to catch out of bounds errors early.

--- include/wdp/Search.h
+++ include/wdp/Search.h
@@ -24,7 +24,8 @@
 
 namespace wdp {
 
-/// Calculate the sum of squared differences between the pixels in two blocks.
+/// Calculate the sum of squared differences between the pixels in two
+/// neighbourhoods of equal size.
 template <typename LHSView, typename RHSView>
 std::size_t sumOfSquaredDifferences(const LHSView &LHS, const RHSView &RHS) {
   // Input arguments must be two compatible 2D image views.
@@ -55,27 +56,79 @@ std::size_t sumOfSquaredDifferences(const LHSView &LHS, const RHSView &RHS) {
   return Sum;
 }
 
-/// Get a block of pixels with given radii surrounding the given coordinates.
+/// Get the neighbourhood of pixels which surround the given coordinates within
+/// the given radii.
 template <typename Image, typename Coordinates, typename Radii>
-const typename Image::view_t getBlock(const Image &Img, const Coordinates &C,
-                                      const Radii &R) {
+const typename Image::view_t
+getNeighbourhood(const Image &Img, const Coordinates NeighbourhoodCoords,
+                 const Radii NeighbourhoodRadii) {
   boost::function_requires<boost::gil::RandomAccess2DImageConcept<Image>>();
   boost::function_requires<boost::gil::Point2DConcept<Coordinates>>();
   boost::function_requires<boost::gil::Point2DConcept<Radii>>();
+
+  // Check that the neighbourhood would be within the boundaries of the image.
+  assert(NeighbourhoodCoords.x - NeighbourhoodRadii.x >= 0);
+  assert(NeighbourhoodCoords.y - NeighbourhoodRadii.y >= 0);
+  assert(NeighbourhoodCoords.x + NeighbourhoodRadii.x < Img.width());
+  assert(NeighbourhoodCoords.y + NeighbourhoodRadii.y < Img.height());
 
   typedef typename Image::view_t View;
   typedef typename Image::point_t Point;
   typedef typename View::xy_locator Locator;
 
-  // Create the dimensions of the block.
-  const Point Dimensions(R.x * 2 + 1, R.y * 2 + 1);
+  // Calculate the dimensions of the neighbourhood.
+  const Point Dimensions(NeighbourhoodRadii.x * 2 + 1,
+                         NeighbourhoodRadii.y * 2 + 1);
 
-  // Get a locator for the beginning of the block.
-  const Point Begin(C.x - R.x, C.y - R.y);
+  // Get a locator for the beginning of the neighbourhood.
+  const Point Begin(NeighbourhoodCoords.x - NeighbourhoodRadii.x,
+                    NeighbourhoodCoords.y - NeighbourhoodRadii.y);
   const Locator &Loc = Img._view.xy_at(Begin);
 
   // Construct a new view into the image.
   return View(Dimensions, Loc);
+}
+
+// Find the neighbourhood in the RHS image which best matches a neighbourhood -
+// defined by the given coordinates and radii - in the LHS image. Search within
+// the given search radii.
+template <typename LHSImage, typename RHSImage, typename Coordinates,
+          typename Radii>
+const Coordinates findBestMatch(const LHSImage &LHSImg, const RHSImage &RHSImg,
+                                const Coordinates NeighbourhoodCoords,
+                                const Radii NeighbourhoodRadii,
+                                const Radii SearchRadii) {
+  boost::function_requires<boost::gil::RandomAccess2DImageConcept<LHSImage>>();
+  boost::function_requires<boost::gil::RandomAccess2DImageConcept<RHSImage>>();
+  boost::function_requires<boost::gil::Point2DConcept<Coordinates>>();
+  boost::function_requires<boost::gil::Point2DConcept<Radii>>();
+
+  const auto LHS =
+      getNeighbourhood(LHSImg, NeighbourhoodCoords, NeighbourhoodRadii);
+
+  // Calculate the bounds of the search.
+  const Coordinates Begin(NeighbourhoodCoords.x - SearchRadii.x,
+                          NeighbourhoodCoords.y - SearchRadii.y);
+  const Coordinates End(NeighbourhoodCoords.x + SearchRadii.x,
+                        NeighbourhoodCoords.y + SearchRadii.y);
+
+  // Search through neighbourhoods in the given search radii to find the lowest
+  // sum of squared differences to the LHS neighbourhood.
+  Coordinates MinCoords(NeighbourhoodCoords);
+  std::size_t MinSSD = (std::size_t)-1;
+  for (auto Y = Begin.y; Y <= End.y; ++Y) {
+    for (auto X = Begin.x; X <= End.x; ++X) {
+      Coordinates RHSCoords(X, Y);
+      const auto RHS = getNeighbourhood(RHSImg, RHSCoords, NeighbourhoodRadii);
+      std::size_t SSD = sumOfSquaredDifferences(LHS, RHS);
+      if (SSD < MinSSD) {
+        MinCoords = RHSCoords;
+        MinSSD = SSD;
+      }
+    }
+  }
+
+  return MinCoords;
 }
 
 } // namespace wdp
